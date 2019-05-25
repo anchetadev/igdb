ad94876521fd036d8d49a81196e0aa7b9cb342d4
Angel Ancheta <angelancheta98@yahoo.com>
2019-05-25 13:18:28 -0700

    created Nav and working on a modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import './App.css';
+import { React, Component } from "react";
+import "./App.css";
 
-import Game from './components/games'
+import Game from "./components/games";
+import GameNavbar from "./components/navbar";
+import DashHeader from "./components/dashHeader";
+import Modal from "./components/modal"
 
-function App() {
-  return (
-    <div className="App">
-      {/* add a nav bar and some content will play around with igdb api to see what kind of data comes through */}
-      {/* interested in how it knows what to show for recommended games, from site it doesn't look very connected to game being 
+class App extends Component() {
+  state = {
+    modalShow: false
+  };
+  handleShow = () =>{
+    this.setState({ show: true })
+  }
+  componentDidMount = () => {
+    setTimeout(function() {
+      this.handleShow()
+    })
+  }
+  render() {
+    let modalClose = () => this.setState({ modalShow: false });
+    return (
+      <div className="App">
+        {/* add a nav bar and some content will play around with igdb api to see what kind of data comes through */}
+        {/* interested in how it knows what to show for recommended games, from site it doesn't look very connected to game being 
         viewed */}
+
+        <GameNavbar />
+        <DashHeader />
+        <Modal
+          show={this.state.modalShow}
+          onHide={modalClose}
+        />
         <Game />
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/dashHeader.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function dashHeader() {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Geeking out is definitely encouraged</h1>
+        <p class="lead">
+          Have fun!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default dashHeader;
